96faac85c716dc4a8c3551ce263d0a88919e03ac
Marvin <marvinlimson2007@gmail.com>
2019-03-23 06:07:49 +0000

    Added in states in app component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render(){
+    //This function is optional
+    constructor(props) {
+        //We're extending from React props. this is required
+        super(props);
+        
+        //Initializing State
+        this.state = {
+            lat: 'Very High',
+            long: null
+        };
+    }
+
+    //Render method has to be defined
+    render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
             err => console.log(err)
         );
-        return <div>Latitude: </div>
+
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
